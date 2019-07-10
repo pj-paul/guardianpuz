26b36e7febf3d8d45e55c0e9da6c0b4012ec047d
PJ Paul <pjp.projects.2019@gmail.com>
2019-07-10 21:41:22 +0530

    Added functionality to capture and pass through solutions

--- guardianpuz.py
+++ guardianpuz.py
@@ -72,7 +72,7 @@ class Clue:
     def __init__(self, number, direction, text):
         self.number = number
         self.direction = direction
-        self.text = text.encode('iso-8859-1', 'ignore') #puzpy doesn't do unicode, even though it's 2019
+        self.text = text #Removing the encoding as per Peter's suggestion
 
     def __lt__(self, other):
         if self.number == other.number:
@@ -84,15 +84,16 @@ class Clue:
             return self.number < other.number
 
 # Helper function to create the crossword blank position list 
-def fill_blanks(blank_matrix, position_vector, clue_direction, clue_length):
+def fill_grid(solution_matrix, position_vector, clue_direction, clue_length, clue_solution):
 
 	'''
-	Function to identify the blank elements of a 15 * 15 crossword grid. 
-	Takes in four arguments:
-	1. blank_matrix: A 15*15 numpy matrix to indicate blank values
+	Function to fill a 15 * 15 crossword grid. 
+	Takes in five arguments:
+	1. solution_matrix: A 15*15 numpy character matrix to hold solutions and blank value indicators.
 	2. position_vector: The starting position from which a clue is being filled. Should be of the form [x,y] where x and y are integers
 	3. clue_direction: Down or Across
 	4. clue_length: How many cells of the matrix does the answer to the clue fill. 
+	5. clue_solution: Final solution of the clue. 
 	'''
 
 	def check_position_vector(position_vector):
@@ -102,40 +103,44 @@ def fill_blanks(blank_matrix, position_vector, clue_direction, clue_length):
 		begin_index = position_vector['x']
 		end_index = position_vector['x'] + clue_length
 		row_index = position_vector['y']
-		blank_matrix[row_index,begin_index:end_index] = 1
+		solution_matrix[row_index,begin_index:end_index] = list(clue_solution)
 	else:
 		begin_index = position_vector['y']
 		end_index = position_vector['y'] + clue_length
 		col_index = position_vector['x']
-		blank_matrix[begin_index:end_index, col_index] = 1
+		solution_matrix[begin_index:end_index, col_index] = list(clue_solution)
 		
 	
 
 clues = []
-blank_matrix = np.zeros([15,15],dtype=int)
+solution_matrix = np.array(['.']*225, dtype=object).reshape([15,15]) #Create a 15*15 grid populated with '.'s.
 
 for clue in json_data['entries']:
+
+	#Generate parameters for 'Clue' class instances and fill_grid function calls. 
 	clue_number = clue['number']
 	clue_direction = "D" if clue['direction'] == "down" else "A"
 	clue_length = clue['length']
 	clue_text = clue['clue']
-	clues.append(Clue(clue_number, clue_direction, clue_text))
-
-	# Edit the blanks matrix 
 	position_vector = clue['position']
-	#print(clue['position'],clue_direction, clue_length)
-	fill_blanks(blank_matrix, position_vector,clue_direction, clue_length )
-	#print(blank_matrix)
+	clue_solution = clue['solution']
 
-blank_matrix = blank_matrix.flatten().tolist() # Flatten the blank matrix into a single row, and then covert into a list. 
-blank_matrix = list(map(str, blank_matrix)) # Convert the blank matrix list of integers into strings.
-fill = [cell.replace('1', '-').replace('0', '.') for cell in blank_matrix]  # Replace '1' with -, and '0' with . to pass onto puz. 
-fill = ''.join(fill) #Convert the list of "-" and "." into a single string. This is what puz accepts as input. 
+	# Pass on the clues to separate class instances
+	clues.append(Clue(clue_number, clue_direction, clue_text))
+	# Invoke fill_grid function with earlier generated parameters
+	fill_grid(solution_matrix, position_vector,clue_direction, clue_length, clue_solution)
+
+
+solution_matrix = solution_matrix.flatten().tolist() # Flatten the solution matrix into a single row, and then covert into a list. 
+solution_string = ''.join(solution_matrix) # Turn the solution_matrix list into one single string. 
+										# This string will has filled in solutions + period for blanks. 
+fill_string = re.sub(r'[A-Za-z]','-',solution_string)  #Substitute alphabets with - (hypehn), and leave the periods alone. 
 
 sorted_clue_texts = list(map(lambda c: c.text, sorted(clues)))
 
 ## Pass the blanks and clues to puz and save the puzzle file. 
-p.fill = fill
+p.fill = fill_string
 p.clues = sorted_clue_texts
-p.solution = fill.replace("-", "A")
-p.save('output.puz')
+p.solution = solution_string
+save_file_name = "Guardian " + json_data['name'] + '.puz'
+p.save(save_file_name)
